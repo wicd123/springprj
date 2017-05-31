0ffeceb262f5afbcc1a3a9df6afb644c90ba763f
goall <wicd123@naver.com>
2017-05-31 15:31:07 +0900

    e

--- src/main/java/java24/board/infc/IBoard.java
+++ src/main/java/java24/board/infc/IBoard.java
@@ -1,18 +1,57 @@
 package java24.board.infc;
 
-
-
-import org.apache.commons.collections.map.HashedMap;
+import java.util.HashMap;
+import java.util.List;
 
 import java24.board.model.ModelArticle;
+import java24.board.model.ModelAttachfile;
+import java24.board.model.ModelBoard;
+import java24.board.model.ModelComments;
 
 public interface IBoard {
 	
 	//Article
-	int getArticleTotalRecord(HashedMap boardcd);
-	ModelArticle getArticleList(hashmap boardcd);
+	int getArticleTotalRecord(HashMap boardcd);
+	
+	List<ModelBoard> getArticleList(HashMap boardcd);
+	
 	ModelArticle getArticle(int articleNo);
+	
 	int insertArticle(ModelArticle boardcd);
+	
+	int updateArticle(HashMap updateValue);
+	
+	int deleteArticle(ModelArticle boardcd);
+	
+	int increaseHit(int articleNo);
+	
+	ModelArticle getNextArticle(HashMap boardcd);
+	
+	ModelArticle getPrevArticle(HashMap boardcd);
+	
+	ModelAttachfile getAttachFile(int attachFileNo);
+	
+	List<ModelBoard> getAttachFileList(int articleNo);
+	
+	int insertAttachFile(ModelAttachfile filename);
+	
+	int deleteAttachFile(ModelAttachfile attachfileno);
+	
+	int getComment(ModelComments commentNO);
+	
+	List <ModelBoard>  getCommentList(ModelComments articleNo);
+	
+	int insertComment(ModelComments articleno);
+	
+	int updateComment();
+	
+	int deleteComment(ModelComments commentno);
+	
+	
+	
+	
+	
+	
 	
 	
 	
